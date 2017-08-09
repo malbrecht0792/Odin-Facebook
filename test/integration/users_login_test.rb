ab252c5cb967e1488abc613d79fa9f6a8e23cdc1
Marcel <malbrecht0792@gmail.com>
2017-08-09 19:07:18 +0200

    Added a few more integration tests

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  	include Devise::Test::IntegrationHelpers
+
+  	def setup
+		@user = users(:marcel)
+	end
+
+  	test 'login in with invalid info' do 
+  		get new_user_session_path
+  		assert_template 'devise/sessions/new'
+  		post new_user_session_path, params: { session: { email: "", 
+  													  password: ""}}
+  		assert_template 'devise/sessions/new'
+  		assert_not flash.empty?
+  	end
+
+  	test 'login with valid info' do
+  		get new_user_session_path
+  		assert_template 'devise/sessions/new'
+  		post new_user_session_path, params: { session: { email: @user.email,
+	                                          password: 'password' } }
+  		assert_redirected_to @user
+  	end
+end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,18 +6,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 	test "invalid signup information" do
   		get new_user_registration_path
     	assert_no_difference 'User.count' do
-      		post '/users', params: { user: { name:   "",
-                                        email: 		  "user@invalid",
+      		post '/users', params: { user: { email:   "user@invalid",
                                         password:              "foo",
                                         password_confirmation: "bar" } }
   		end
-  		assert_template 'users/new'
+  		assert_template 'devise/registrations/new'
     	assert_select 'div#error_explanation'
    	 	assert_select 'div.field_with_errors'
 	end
 
-	# test "valid signup information" do
-	# 	get new_user_registration_path
-	# 	assert_difference
-	# end
+	test "valid signup information" do
+		get new_user_registration_path
+		assert_difference 'User.count', 1 do
+			post '/users', params: { user: { email:   "user@example.com",
+                                        password:              "password",
+                                        password_confirmation: "password" } }
+		end
+		#update this later to include the proper template
+		#couldn't get it to work because it was rendering with []
+		#maybe try this: https://github.com/plataformatec/devise/wiki/How-To:-Redirect-to-a-specific-page-on-successful-sign-up-(registration)
+		assert_response :redirect
+	end
+
 end
