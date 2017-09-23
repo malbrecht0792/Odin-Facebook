ca52e85a250435c7a0fceca380f67db119f4d13c
Marcel <malbrecht0792@gmail.com>
2017-09-22 19:24:08 -0500

    Adding aws timeline photo upload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,17 @@ Rails.application.configure do
   #Devise default url options
   config.action_mailer.default_url_options = { host: 'localhost', port: 3040 }
 
-  Paperclip.options[:command_path] = "/usr/local/bin/"
+  #Paperclip.options[:command_path] = "/usr/local/bin/"
+
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
 
 end
