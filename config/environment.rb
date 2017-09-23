d10dd72f4b8f3dd715529fb4b4c487b5f207ad93
Marcel <malbrecht0792@gmail.com>
2017-09-23 12:32:10 -0500

    Updated gitignore file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 # Load the Rails application.
 require_relative 'application'
 
+require File.join(File.dirname(__FILE__), 'boot')
+
+# Load heroku vars from local file
+heroku_env = File.join(Rails.root, 'config', 'heroku_env.rb')
+load(heroku_env) if File.exists?(heroku_env)
+
+Rails::Initializer.run do |config|
+
 # Initialize the Rails application.
 Rails.application.initialize!
