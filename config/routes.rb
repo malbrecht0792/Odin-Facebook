86d688b1dd9770fdf5a2c05851629712f2a7fada
Marcel <malbrecht0792@gmail.com>
2017-10-04 18:38:31 -0500

    Updated routes to fix devise error message

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'users#show'
+  authenticated :user do
+    root to: 'users#show', as: :authenticated_root
+  end
+  root to: redirect('/users/sign_in')
 
   devise_for :users, :controllers => { registrations: 'users/registrations', 
   									   :omniauth_callbacks => "users/omniauth_callbacks" }
