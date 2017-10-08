c5464aa104c99f0bf0d892afa1837e6bbaa8302c
Marcel <malbrecht0792@gmail.com>
2017-10-08 09:46:38 -0500

    Update login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable,
-     	   :recoverable, :rememberable, :trackable, :validatable, 
+     	   :recoverable, :trackable, :validatable, 
      	   :omniauthable, :omniauth_providers => [:facebook]
 
   	has_many :posts, foreign_key: "author_id"
