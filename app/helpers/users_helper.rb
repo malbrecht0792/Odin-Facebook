0f259198feeadae1fd0689597630ba9aaefdcc85
Marcel <malbrecht0792@gmail.com>
2018-01-20 14:53:18 -0600

    Broke out code into partials

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,5 +7,9 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.first_name, class: "gravatar")
   end
+
+  def full_name(user)
+    "#{user.first_name} #{user.last_name}"
+  end
   
 end
